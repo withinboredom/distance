55fee07fdc66cef720bfb3d6c294a2456cea2ebd
Robert Landers <landers.robert@gmail.com>
2024-07-10 18:57:10 +0200

    allow creating your own implementation

--- src/TimeUnit.php
+++ src/TimeUnit.php
@@ -2,14 +2,16 @@
 
 namespace Withinboredom\Time;
 
-enum TimeUnit: int
-{
-    case Nanoseconds = 1;
-    case Microseconds = 1000;
-    case Milliseconds = 1000000;
-    case Seconds = 1000000000;
-    case Minutes = 60000000000;
-    case Hours = 3600000000000;
-    case Days = 86400000000000;
-    case Weeks = 604800000000000;
+if(!class_exists(TimeUnit::class)) {
+    enum TimeUnit: int
+    {
+        case Nanoseconds = 1;
+        case Microseconds = 1000;
+        case Milliseconds = 1000000;
+        case Seconds = 1000000000;
+        case Minutes = 60000000000;
+        case Hours = 3600000000000;
+        case Days = 86400000000000;
+        case Weeks = 604800000000000;
+    }
 }
